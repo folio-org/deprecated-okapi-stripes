351732c6e23cc62594b6add78891601bdb84bc17
Wolfram Schneider <wosch@indexdata.com>
2016-09-01 12:46:09 +0000

    big refactoring

--- bin/install.sh
+++ bin/install.sh
@@ -5,31 +5,12 @@ set -e
 
 : ${interactive="yes"}
 
-(
-cd stripes-connect
-npm install
-)
+stripes_repo=$(pwd)/../stripes-experiments
 
 (
-mkdir -p stripes-core/node_modules && \
-   cd stripes-core/node_modules/ && \
-   rm -rf stripes-loader && \
-   rm -rf stripes-loader-[0-9]* && \
-   wget -q https://s3.amazonaws.com/folio-ui-bundle/tarball/stripes-loader-0.0.0-nogit.tgz && \
-   tar xfz stripes-loader-0.0.0-nogit.tgz && \
-   sed -i.bak -e 's/^[ \t]*"stripes-loader"[ \t]*:.*//' ../package.json
-)
-
-(
+cd $stripes_repo
 cd stripes-core
 npm install
-)
-
-(
-cd stripes-core/node_modules
-if [ ! -e "@stripes-experiments" ]; then
-  ln -fs ../.. @stripes-experiments
-fi
 )
 
 if [ "$interactive" = "yes" ]; then
