0391c9348095b4b589c884dad90d660096b0a8e1
Wolfram Schneider <wosch@indexdata.com>
2016-09-28 12:38:24 +0000

    our npm repo was renamed to repository.folio.org, FOLIO-337

--- bin/install.sh
+++ bin/install.sh
@@ -8,8 +8,8 @@ set -e
 stripes_repo=$(pwd)/../stripes-experiments
 
 # configure ~/.npmrc to use indexdata npm repo for folio
-npm config set @folio:registry https://folio-nexus.indexdata.com/repository/npm-folio/
-npm config set @folio-sample-modules:registry https://folio-nexus.indexdata.com/repository/npm-folio/
+npm config set @folio:registry https://repository.folio.org/repository/npm-folio/
+npm config set @folio-sample-modules:registry https://repository.folio.org/repository/npm-folio/
 
 
 # okaip-stripes
