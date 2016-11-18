731cc66bbbff5279899fa2303987a68c2bea7f1c
Wolfram Schneider <wosch@indexdata.com>
2016-11-18 17:33:15 +0000

    support both "ui" and non ui modules
    is_ui_module=false tenant=test4 ./bin/ui-deploy.sh

--- bin/ui-deploy.sh
+++ bin/ui-deploy.sh
@@ -5,13 +5,15 @@
 #
 # e.g.:
 #
-# $ tenant=test module="trivial trivial-okapi" ./ui-deploy.sh
+# $ tenant=test modules="trivial @folio-sample-modules/trivial-okapi" ./ui-deploy.sh
 #
 
-#set -e
+set -e
 
 : ${tenant="test"}
-: ${modules="trivial trivial-okapi patrons"}
+: ${modules="trivial @folio-sample-modules/trivial-okapi https://s3.amazonaws.com/folio-ui-bundle/tarball/trivial-okapi.tgz"}
+: ${is_ui_module=true}
+
 curl='curl -sSf'
 
 module_json=$(mktemp)
@@ -40,42 +42,62 @@ $curl -w '\n' -X POST -D - \
 #
 for module in $modules
 do
+    case $module in
+        @* ) npm="$module"; url="" ;;
+        * )  npm=""; url="$module" ;;
+    esac
+ 
+    # https://s3.amazonaws.com/folio-ui-bundle/tarball/trivial-okapi.tgz"
+    id=$(echo $module | perl -npe 's,https?://,,i; s/\.tgz$//')
     
-  # trivial module
-  cat > $module_json <<END
+    # remove @ and / from id   
+    id=$(echo $id | perl -npe 's/^@//; s,/,-,g')
+    
+    # trivial module
+    if $is_ui_module; then
+    cat > $module_json <<END
 {
-  "id" : "$module",
+  "id" : "$id",
   "name" : "$module",
   "uiDescriptor" : {
-     "npm" : "$module"
+     "npm" : "$npm",
+     "url" : "$url"
   }
 }
 END
-
-  echo ""
-  echo "==> Create module '$module'"
-  $curl -w '\n' -X POST -D - \
-    -H "Content-type: application/json" \
-    -d @$module_json  \
-    http://localhost:9130/_/proxy/modules
-
-  # Enable tenant
-  tenant_enable_json=$(mktemp)
-  cat > $tenant_enable_json <<END
+    else
+        cat > $module_json <<END
 {
-  "id" : "$module"
+  "id" : "$id",
+  "name" : "$module"
+}
+END
+    fi
+
+    echo ""
+    echo "==> Create module '$module'"
+    $curl -w '\n' -X POST -D - \
+      -H "Content-type: application/json" \
+      -d @$module_json  \
+      http://localhost:9130/_/proxy/modules
+  
+    # Enable tenant
+    tenant_enable_json=$(mktemp)
+    cat > $tenant_enable_json <<END
+{
+  "id" : "$id"
 }
 END
 
-  echo ""
-  echo "==> Enable module '$module' for tenant '$tenant'"
-  $curl -w '\n' -X POST -D - \
-    -H "Content-type: application/json" \
-    -d @$tenant_enable_json  \
-    http://localhost:9130/_/proxy/tenants/$tenant/modules
-
-  # get full info for trivial (repeat for each one returned above)
-  $curl -w '\n' -D - http://localhost:9130/_/proxy/modules/$module
+    echo ""
+    echo "==> Enable module '$id' for tenant '$tenant'"
+    $curl -w '\n' -X POST -D - \
+      -H "Content-type: application/json" \
+      -d @$tenant_enable_json  \
+      http://localhost:9130/_/proxy/tenants/$tenant/modules
+  
+    # get full info for trivial (repeat for each one returned above)
+    $curl -w '\n' -D - http://localhost:9130/_/proxy/modules/$id
 done
 
 # get list of enabled modules for tenant
