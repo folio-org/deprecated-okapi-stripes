92f88449ad61ec55d368fac206d1f5331543e516
Wolfram Schneider <wosch@indexdata.com>
2016-11-18 20:53:41 +0000

    refactor as a wrapper to ui-deploy.sh

--- bin/ui-deploy-demo.sh
+++ bin/ui-deploy-demo.sh
@@ -7,134 +7,17 @@
 #
 # create two tenants, with ui and non-ui modules
 #
-# $ tenant="demo" modules_ui="patrons https://s3.amazonaws.com/folio-ui-bundle/tarball/trivial-wolfram.tgz" modules="trivial trivial-okapi" ./ui-deploy-demo.sh
+# $ tenant="demo" modules_ui="trivial https://s3.amazonaws.com/folio-ui-bundle/tarball/trivial-okapi.tgz" modules="trivial trivial-okapi" ./ui-deploy-demo.sh
 #
 
-#set -e
+set -e
 
 : ${tenant="demo"}
-: ${modules_ui="patrons https://s3.amazonaws.com/folio-ui-bundle/tarball/trivial-okapi.tgz"}
-: ${modules="trivial"}
-curl='curl -sSf'
+: ${modules_ui="trivial @folio-sample-modules/trivial-okapi"}
+: ${modules="https://s3.amazonaws.com/folio-ui-bundle/tarball/trivial-okapi.tgz"}
 
-module_json=$(mktemp)
-tenant_json=$(mktemp)
+program=$(dirname $0)/ui-deploy.sh
 
-########################################
-# same 1st tenant as manual
-#
-cat > $tenant_json <<END
-{
-  "id" : "$tenant",
-  "name" : "$tenant library",
-  "description" : "$tenant Library"
-}
-END
+tenant=$tenant is_ui_module=true  modules="$modules_ui" $program
+tenant=$tenant is_ui_module=false modules="$modules"    $program
 
-echo "==> Create tenant '$tenant'"
-$curl -w '\n' -X POST -D - \
-  -H "Content-type: application/json" \
-  -d @$tenant_json \
-  http://localhost:9130/_/proxy/tenants
-
-
-########################################
-# ui modules
-#
-for module in $modules_ui
-do
-    id=$(basename $module .tgz)
-        
-    # trivial module
-    cat > $module_json <<END
-{
-  "id" : "$id",
-  "name" : "$id",
-  "uiDescriptor" : {
-     "npm" : "$module",
-     "url" : "$module"
-  }
-}
-END
-
-    echo ""
-    echo "==> Create module '$id'"
-    $curl -w '\n' -X POST -D - \
-      -H "Content-type: application/json" \
-      -d @$module_json  \
-      http://localhost:9130/_/proxy/modules
-  
-    # Enable tenant
-    tenant_enable_json=$(mktemp)
-    cat > $tenant_enable_json <<END
-{
-  "id" : "$id"
-}
-END
-
-    echo ""
-    echo "==> Enable ui module '$id' for tenant '$tenant'"
-    $curl -w '\n' -X POST -D - \
-      -H "Content-type: application/json" \
-      -d @$tenant_enable_json  \
-      http://localhost:9130/_/proxy/tenants/$tenant/modules
-  
-    # get full info for trivial (repeat for each one returned above)
-    $curl -w '\n' -D - http://localhost:9130/_/proxy/modules/$id
-done
-
-########################################
-# non-ui modules
-#
-for module in $modules
-do
-    
-    # trivial module
-    cat > $module_json <<END
-{
-  "id" : "$module",
-  "name" : "$module"
-}
-END
-
-    echo ""
-    echo "==> Create module '$module'"
-    $curl -w '\n' -X POST -D - \
-      -H "Content-type: application/json" \
-      -d @$module_json  \
-      http://localhost:9130/_/proxy/modules
-  
-    # Enable tenant
-    tenant_enable_json=$(mktemp)
-  cat > $tenant_enable_json <<END
-{
-  "id" : "$id"
-}
-END
-
-    echo ""
-    echo "==> Enable non-ui module '$module' for tenant '$tenant'"
-    $curl -w '\n' -X POST -D - \
-      -H "Content-type: application/json" \
-      -d @$tenant_enable_json  \
-      http://localhost:9130/_/proxy/tenants/$tenant/modules
-  
-    # get full info for trivial (repeat for each one returned above)
-    $curl -w '\n' -D - http://localhost:9130/_/proxy/modules/$module
-done
-
-# get list of enabled modules for tenant
-echo ""
-echo "==> List modules for '$tenant'"
-$curl -w '\n' -D - http://localhost:9130/_/proxy/tenants/$tenant/modules
-
-# show module config
-echo ""
-for module in $($curl -w '\n' -D - http://localhost:9130/_/proxy/tenants/$tenant/modules |
-    egrep '"id"' |awk '{print $3}' | sed -e 's/"//g')
-do
-    $curl -w '\n'  http://localhost:9130/_/proxy/modules/$module
-done
-    
-
-rm -f $module_json $tenant_json $tenant_enable_json
