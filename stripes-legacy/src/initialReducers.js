e3706d456d529db46c2d83da4deab6273d323ba5
Wolfram Schneider <wosch@indexdata.com>
2016-12-06 13:11:25 +0000

    remove stale code

--- stripes-legacy/src/initialReducers.js
+++
@@ -1,8 +0,0 @@
-import { routerReducer } from 'react-router-redux';
-import { combineReducers } from 'redux';
-import { reducer as form } from 'redux-form';
-
-export default {
-  routing: routerReducer,
-  form: form
-};
