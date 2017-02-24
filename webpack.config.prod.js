41d70b7cca91e366901dabc442d1b84124118e24
Wolfram Schneider <wosch@indexdata.com>
2017-02-24 17:56:41 +0000

    webpack2 updates, synced from stipes-core

--- webpack.config.prod.js
+++ webpack.config.prod.js
@@ -1,20 +1,21 @@
 // Common Webpack configuration for building Stripes for production
 
 const webpack = require('webpack');
+const path = require('path');
 const CopyWebpackPlugin = require('copy-webpack-plugin');
 const ExtractTextPlugin = require("extract-text-webpack-plugin");
 
+const bootstrapDist = require.resolve('bootstrap/package.json').replace('package.json', 'dist');
+
 module.exports = {
   plugins: [
-    new webpack.DefinePlugin({
-      'OKAPI_URL': false,
-      'process.env': {
-        'NODE_ENV': JSON.stringify('production')
-      }
-    }),
-    
-    new ExtractTextPlugin({filename: "global.css", allChunks: true }),
-    
+    // new webpack.optimize.OccurenceOrderPlugin(),
+    // new webpack.optimize.UglifyJsPlugin({
+    //   compressor: {
+    //     warnings: false
+    //   }
+    // }),
+    new ExtractTextPlugin({filename: 'global.css', allChunks: true }),
     new CopyWebpackPlugin([
       { from:bootstrapDist, to:'bootstrap'},
       { from: path.join(__dirname, 'index.html'), to:'index.html'},
